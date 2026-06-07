920e8923975946280923342e9dec7405b590346c
Chris Lorenzo <chiefcll@gmail.com>
2026-06-07 11:39:47 -0400

    Updates

---
+++ assets/FlexGrow-BD7W7XPJ.js
@@ -0,0 +1,127 @@
+import { b as createSignal, o as onMount, s as setGlobalBackground, l as createComponent, C as Column, c as createElement, n as insertNode, p as createTextNode, a as setProp, R as Row, O as styles, A as spread, q as mergeProps } from "./index-C8HFTxb9.js";
+
+const FlexGrow = () => {
+    const RowStyles = {
+        display: "flex",
+        justifyContent: "flexStart",
+        width: 1600,
+        height: 110,
+        color: 4294967295
+    };
+    const rowTitle = {
+        fontSize: 44,
+        marginTop: 25,
+        marginBottom: -20,
+        skipFocus: true
+    };
+    const red = 4278190335;
+    const darkorange = 4287365375;
+    const green = 16711935;
+    function Block(props) {
+        const styles2 = {
+            width: props.flexGrow ? 0 : 200,
+            height: 100,
+            y: 5
+        };
+        return (() => {
+            var _el$ = createElement("view");
+            spread(_el$, mergeProps(props, {
+                style: styles2
+            }), false);
+            return _el$;
+        })();
+    }
+    const [columnY, setColumnY] = createSignal(50);
+    function onFocus() {
+        this.children[this.selected || 0].setFocus();
+        setColumnY(150 + (this.y || 0) * -1);
+    }
+    onMount(() => {
+        setGlobalBackground(858993663);
+    });
+    const gap = 50;
+    return createComponent(Column, {
+        x: 160,
+        get y() {
+            return columnY();
+        },
+        gap: 30,
+        height: 850,
+        get width() {
+            return RowStyles.width;
+        },
+        get style() {
+            return styles.Column;
+        },
+        get children() {
+            return [ (() => {
+                var _el$2 = createElement("text");
+                insertNode(_el$2, createTextNode("Flex Start RTL"));
+                setProp(_el$2, "style", rowTitle);
+                return _el$2;
+            })(), createComponent(Row, {
+                gap: gap,
+                direction: "rtl",
+                style: RowStyles,
+                onFocus: onFocus,
+                get children() {
+                    return [ createComponent(Block, {
+                        flexGrow: 1,
+                        autofocus: true,
+                        color: red
+                    }), createComponent(Block, {
+                        flexGrow: 2,
+                        color: darkorange
+                    }), createComponent(Block, {
+                        color: green
+                    }) ];
+                }
+            }), (() => {
+                var _el$4 = createElement("text");
+                insertNode(_el$4, createTextNode("Flex End with Flex Grow"));
+                setProp(_el$4, "style", rowTitle);
+                return _el$4;
+            })(), createComponent(Row, {
+                gap: gap,
+                style: RowStyles,
+                onFocus: onFocus,
+                get children() {
+                    return [ createComponent(Block, {
+                        color: green
+                    }), createComponent(Block, {
+                        flexGrow: 1,
+                        color: red
+                    }), createComponent(Block, {
+                        flexGrow: 2,
+                        color: darkorange
+                    }) ];
+                }
+            }), (() => {
+                var _el$6 = createElement("text");
+                insertNode(_el$6, createTextNode("Space Between with Flex Grow"));
+                setProp(_el$6, "style", rowTitle);
+                return _el$6;
+            })(), createComponent(Row, {
+                gap: gap,
+                justifyContent: "spaceBetween",
+                style: RowStyles,
+                onFocus: onFocus,
+                get children() {
+                    return [ createComponent(Block, {
+                        flexGrow: 1,
+                        color: red
+                    }), createComponent(Block, {
+                        flexGrow: 1,
+                        flexOrder: -1,
+                        color: darkorange
+                    }), createComponent(Block, {
+                        flexGrow: 2,
+                        color: green
+                    }) ];
+                }
+            }) ];
+        }
+    });
+};
+
+export { FlexGrow as default };
---
+++ assets/TMDBGrid-Cpdgx_K0.js
@@ -0,0 +1,242 @@
+import { b as createSignal, i as createEffect, w as untrack, x as createMemo, c as createElement, y as chainRefs, z as hasFocus, v as use, A as spread, q as mergeProps, B as chainFunctions, k as insert, l as createComponent, I as Index, E as ElementNode, D as debounce, s as setGlobalBackground, j as on, G as activeElement, n as insertNode, a as setProp, p as createTextNode, H as ContentBlock, P as Poster } from "./index-C8HFTxb9.js";
+
+function Grid(props) {
+    const [focusedIndex, setFocusedIndex] = createSignal(0);
+    const baseColumns = 4;
+    createEffect(() => {
+        var _a;
+        const currentIndex = untrack(focusedIndex);
+        if (props.selected === currentIndex) return;
+        if (props.selected !== void 0 && ((_a = props.items) == null ? void 0 : _a.length) > props.selected) {
+            moveFocus(props.selected - currentIndex);
+        }
+    });
+    const itemWidth = () => {
+        var _a;
+        return (_a = props.itemWidth) != null ? _a : 300;
+    };
+    const itemHeight = () => {
+        var _a;
+        return (_a = props.itemHeight) != null ? _a : 300;
+    };
+    const columns = createMemo(() => props.columns || baseColumns);
+    const totalWidth = createMemo(() => {
+        var _a;
+        return itemWidth() + ((_a = props.itemOffset) != null ? _a : 0);
+    });
+    const totalHeight = createMemo(() => {
+        var _a;
+        return itemHeight() + ((_a = props.itemOffset) != null ? _a : 0);
+    });
+    const rows = createMemo(() => Math.ceil(props.items.length / columns()));
+    function focus() {
+        var _a;
+        const focusedElm = gridRef.children[focusedIndex()];
+        if (focusedElm instanceof ElementNode && !hasFocus(focusedElm)) {
+            focusedElm.setFocus();
+            (_a = props.onSelectedChanged) == null ? void 0 : _a.call(gridRef, focusedIndex(), gridRef, focusedElm);
+            return true;
+        }
+        return false;
+    }
+    function moveFocus(delta) {
+        if (!props.items || props.items.length === 0) return false;
+        const newIndex = focusedIndex() + delta;
+        if (newIndex >= 0 && newIndex < props.items.length) {
+            setFocusedIndex(newIndex);
+        } else if (props.looping) {
+            const totalItems = props.items.length;
+            if (delta < 0) {
+                const lastRowStart = totalItems - totalItems % columns() || totalItems - columns();
+                const target = lastRowStart + focusedIndex() % columns();
+                setFocusedIndex(target < totalItems ? target : target - columns());
+            } else {
+                setFocusedIndex(focusedIndex() % columns());
+            }
+        } else {
+            return false;
+        }
+        return focus();
+    }
+    function handleHorizontalFocus(delta) {
+        if (!props.items || props.items.length === 0) return false;
+        const newIndex = focusedIndex() + delta;
+        const isWithinRow = Math.floor(newIndex / columns()) === Math.floor(focusedIndex() / columns());
+        if (newIndex >= 0 && newIndex < props.items.length && isWithinRow) {
+            setFocusedIndex(newIndex);
+        } else if (props.looping) {
+            const rowStart = Math.floor(focusedIndex() / columns()) * columns();
+            const rowEnd = Math.min(rowStart + columns() - 1, props.items.length - 1);
+            setFocusedIndex(delta > 0 ? newIndex > rowEnd ? rowStart : newIndex : newIndex < rowStart ? rowEnd : newIndex);
+        } else {
+            return false;
+        }
+        return focus();
+    }
+    createEffect(() => {
+        if (props.items && props.items.length > 0 && gridRef && hasFocus(gridRef)) {
+            queueMicrotask(focus);
+        }
+    });
+    function scrollToIndex(index) {
+        untrack(() => {
+            if (!props.items || props.items.length === 0) return;
+            if (!hasFocus(gridRef)) {
+                gridRef.setFocus();
+            }
+            const clampedIndex = Math.max(0, Math.min(index, props.items.length - 1));
+            setFocusedIndex(clampedIndex);
+            queueMicrotask(focus);
+        });
+    }
+    const scrollY = createMemo(() => {
+        var _a;
+        return props.scroll === "none" ? (_a = props.y) != null ? _a : 0 : -Math.floor(focusedIndex() / columns()) * totalHeight() + (props.y || 0);
+    });
+    let gridRef;
+    return (() => {
+        var _el$ = createElement("view");
+        var _ref$ = chainRefs(el => gridRef = el, props.ref);
+        typeof _ref$ === "function" && use(_ref$, _el$);
+        spread(_el$, mergeProps(props, {
+            transition: {
+                y: true
+            },
+            get height() {
+                return totalHeight() * rows();
+            },
+            scrollToIndex: scrollToIndex,
+            get onUp() {
+                return chainFunctions(props.onUp, () => moveFocus(-columns()));
+            },
+            get onDown() {
+                return chainFunctions(props.onDown, () => moveFocus(columns()));
+            },
+            get onLeft() {
+                return chainFunctions(props.onLeft, () => handleHorizontalFocus(-1));
+            },
+            get onRight() {
+                return chainFunctions(props.onRight, () => handleHorizontalFocus(1));
+            },
+            get onFocus() {
+                return chainFunctions(props.onFocus, () => handleHorizontalFocus(0));
+            },
+            strictBounds: false,
+            get y() {
+                return scrollY();
+            }
+        }), true);
+        insert(_el$, createComponent(Index, {
+            get each() {
+                return props.items;
+            },
+            children: (item, index) => createComponent(props.children, {
+                get item() {
+                    return item();
+                },
+                index: index,
+                get width() {
+                    return itemWidth();
+                },
+                get height() {
+                    return itemHeight();
+                },
+                get x() {
+                    return index % columns() * totalWidth();
+                },
+                get y() {
+                    return Math.floor(index / columns()) * totalHeight();
+                }
+            })
+        }));
+        return _el$;
+    })();
+}
+
+const TMDB = props => {
+    const [heroContent, setHeroContent] = createSignal({});
+    let contentBlock, solidLogo, firstRun = true;
+    const delayedBackgrounds = debounce(setGlobalBackground, 800);
+    const delayedHero = debounce(content => setHeroContent(content || {}), 600);
+    createEffect(on(activeElement, elm => {
+        if (!elm) return;
+        if (firstRun) {
+            elm.backdrop && setGlobalBackground(elm.backdrop);
+            elm.heroContent && setHeroContent(elm.heroContent);
+            firstRun = false;
+        } else {
+            elm.backdrop && delayedBackgrounds(elm.backdrop);
+            elm.heroContent && delayedHero(elm.heroContent);
+        }
+    }, {
+        defer: true
+    }));
+    const items = createMemo(() => props.data.rows.map(row => row.items()).flat());
+    return [ (() => {
+        var _el$ = createElement("view"), _el$2 = createElement("text"), _el$4 = createElement("view"), _el$5 = createElement("view"), _el$6 = createElement("text");
+        insertNode(_el$, _el$2);
+        insertNode(_el$, _el$4);
+        insertNode(_el$, _el$5);
+        insertNode(_el$, _el$6);
+        var _ref$ = solidLogo;
+        typeof _ref$ === "function" ? use(_ref$, _el$) : solidLogo = _el$;
+        setProp(_el$, "width", 300);
+        setProp(_el$, "height", 150);
+        setProp(_el$, "x", 162);
+        setProp(_el$, "y", 80);
+        setProp(_el$, "zIndex", 105);
+        insertNode(_el$2, createTextNode("Built with"));
+        setProp(_el$2, "x", 80);
+        setProp(_el$2, "fontSize", 28);
+        setProp(_el$2, "color", 4143380121);
+        setProp(_el$4, "y", 32);
+        setProp(_el$4, "src", "./assets/solidWord.png");
+        setProp(_el$4, "width", 280);
+        setProp(_el$4, "height", 52);
+        setProp(_el$5, "x", 0);
+        setProp(_el$5, "y", 110);
+        setProp(_el$5, "src", "./assets/tmdb.png");
+        setProp(_el$5, "width", 80);
+        setProp(_el$5, "height", 41);
+        insertNode(_el$6, createTextNode("This product uses the TMDB API but is not endorsed or certified by TMDB."));
+        setProp(_el$6, "x", 90);
+        setProp(_el$6, "y", 110);
+        setProp(_el$6, "contain", "width");
+        setProp(_el$6, "width", 160);
+        setProp(_el$6, "fontSize", 12);
+        setProp(_el$6, "color", 4143380121);
+        return _el$;
+    })(), createComponent(ContentBlock, {
+        ref(r$) {
+            var _ref$2 = contentBlock;
+            typeof _ref$2 === "function" ? _ref$2(r$) : contentBlock = r$;
+        },
+        y: 300,
+        x: 162,
+        get content() {
+            return heroContent();
+        }
+    }), (() => {
+        var _el$8 = createElement("view");
+        setProp(_el$8, "x", 165);
+        setProp(_el$8, "y", 540);
+        setProp(_el$8, "clipping", true);
+        insert(_el$8, createComponent(Grid, {
+            x: 12,
+            y: 12,
+            get autofocus() {
+                return items();
+            },
+            itemWidth: 200,
+            get items() {
+                return items();
+            },
+            columns: 6,
+            itemOffset: 36,
+            children: props2 => createComponent(Poster, props2)
+        }));
+        return _el$8;
+    })() ];
+};
+
+export { TMDB as default };
