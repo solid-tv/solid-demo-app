bad07960907684ba46fe2b60a8cb34503a2c7fa2
Chris Lorenzo <chiefcll@gmail.com>
2026-08-28 18:26:23 -0400

    Updates

---
+++ assets/state-B_Z-4UWo.js
@@ -0,0 +1,5 @@
+import { q as createSignal } from "./render-B5QjUuTk.js";
+
+var [globalBackground, setGlobalBackground] = createSignal("");
+
+export { setGlobalBackground as n, globalBackground as t };
