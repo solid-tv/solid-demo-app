1d9d7e03d7c4c0d7ec16a64ca83f3c1161e960a5
Chris Lorenzo <chiefcll@gmail.com>
2026-08-29 07:22:30 -0400

    Updates

---
+++ assets/state-legacy-CPHXLYcn.js
@@ -0,0 +1,59 @@
+(function() {
+    function _slicedToArray(r, e) {
+        return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest();
+    }
+    function _nonIterableRest() {
+        throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+    }
+    function _unsupportedIterableToArray(r, a) {
+        if (r) {
+            if ("string" == typeof r) return _arrayLikeToArray(r, a);
+            var t = {}.toString.call(r).slice(8, -1);
+            return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0;
+        }
+    }
+    function _arrayLikeToArray(r, a) {
+        (null == a || a > r.length) && (a = r.length);
+        for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e];
+        return n;
+    }
+    function _iterableToArrayLimit(r, l) {
+        var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"];
+        if (null != t) {
+            var e, n, i, u, a = [], f = !0, o = !1;
+            try {
+                if (i = (t = t.call(r)).next, 0 === l) {
+                    if (Object(t) !== t) return;
+                    f = !1;
+                } else for (;!(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0) ;
+            } catch (r) {
+                o = !0, n = r;
+            } finally {
+                try {
+                    if (!f && null != t.return && (u = t.return(), Object(u) !== u)) return;
+                } finally {
+                    if (o) throw n;
+                }
+            }
+            return a;
+        }
+    }
+    function _arrayWithHoles(r) {
+        if (Array.isArray(r)) return r;
+    }
+    System.register([ "./render-legacy-DmBmFa1k.js" ], function(_export, _context) {
+        "use strict";
+        var createSignal, globalBackground, setGlobalBackground;
+        return {
+            setters: [ function(_renderLegacy001Js) {
+                createSignal = _renderLegacy001Js.q;
+            } ],
+            execute: function execute() {
+                var _createSignal, _createSignal2;
+                _createSignal = createSignal(""), _createSignal2 = _slicedToArray(_createSignal, 2), 
+                globalBackground = _createSignal2[0], setGlobalBackground = _createSignal2[1], _export("t", globalBackground), 
+                _export("n", setGlobalBackground);
+            }
+        };
+    });
+})();
